8ac3eeae30bc05e14b8869b76cd73c8da9bed53e
andrey post <postoliuk.av@gmail.com>
2021-02-02 10:14:24 +0200

    css

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,28 @@
 const { src, dest, watch, parallel, series } = require('gulp'),
-    svgSprite = require('gulp-svg-sprite'),
-    imagemin = require('gulp-imagemin'),
+    htmlmin = require('gulp-htmlmin'),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     concatCss = require('gulp-concat-css'),
     cssnano = require('gulp-cssnano'),
-    // sourcemaps = require('gulp-sourcemaps'),
-    concat = require('gulp-concat'),
     babel = require('gulp-babel'),
-    uglify = require('gulp-uglify')
-// livereload = require('gulp-livereload')
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    svgSprite = require('gulp-svg-sprite'),
+    imagemin = require('gulp-imagemin')
 
-// function delFiles() {
-// return del([
-// 'dist/html', 'dist/css', 'dist/js', 'dist/images'
-// here we use a globbing pattern to match everything inside the `mobile` folder
-// 'dist/mobile/**/*',
-// we don't want to clean this file though so we negate the pattern
-// '!dist/mobile/deploy.json'
-// ]);
-// }
 
 const htmlPath = 'src/*.html', cssPath = 'src/css/*.css', sassPath = 'src/scss/**/*.scss', jsPath = 'src/js/*.js'
 
 function copyHtml() {
-    return src(htmlPath).pipe(dest('./'))
+    return src(htmlPath).pipe(htmlmin({ collapseWhitespace: true })).pipe(dest('./'))
+}
+
+function cssTask() {
+    return src([cssPath, sassPath]).pipe(sass()).pipe(autoprefixer({ cascade: false })).pipe(concatCss('styles.css')).pipe(cssnano()).pipe(dest('./css'));
+}
+
+function jsTask() {
+    return src(jsPath).pipe(babel({ presets: ['@babel/env'] })).pipe(concat('main.js')).pipe(uglify()).pipe(dest('./js'))
 }
 
 function svgTask() {
@@ -55,32 +53,7 @@ function svgTask() {
 }
 
 function imgTask() {
-    return src('src/images/*')
-        .pipe(imagemin())
-        .pipe(dest('./images'))
-}
-
-function cssTask() {
-    return src([cssPath, sassPath])
-        // .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(autoprefixer({ cascade: false }))
-        .pipe(concatCss('styles.css'))
-        .pipe(cssnano())
-        // .pipe(sourcemaps.write('.'))
-        .pipe(dest('./css'));
-}
-
-function jsTask() {
-    return src(jsPath)
-        // .pipe(sourcemaps.init())
-        .pipe(babel({
-            presets: ['@babel/env']
-        }))
-        .pipe(concat('main.js'))
-        .pipe(uglify())
-        // .pipe(sourcemaps.write('.'))
-        .pipe(dest('./js'))
+    return src('src/images/*').pipe(imagemin()).pipe(dest('./images'))
 }
 
 function watchTask() {
@@ -88,16 +61,16 @@ function watchTask() {
 }
 
 exports.copyHtml = copyHtml
-exports.svgTask = svgTask
-exports.imgTask = imgTask
 exports.cssTask = cssTask
 exports.jsTask = jsTask
-exports.default = series(parallel(copyHtml, svgTask, imgTask, cssTask, jsTask), watchTask);
+exports.svgTask = svgTask
+exports.imgTask = imgTask
+exports.default = series(parallel(copyHtml, cssTask, jsTask, svgTask, imgTask), watchTask);
 
 
-if (process.env.NODE_ENV === 'production') {
-    exports.build = series(transpile, minify);
-  } else {
-    exports.build = series(parallel(copyHtml, svgTask, imgTask, cssTask, jsTask), watchTask);
-  }
+// if (process.env.NODE_ENV === 'production') {
+//     exports.build = series(transpile, minify);
+// } else {
+//     exports.build = series(parallel(copyHtml, cssTask, jsTask, svgTask, imgTask), watchTask);
+// }
 
