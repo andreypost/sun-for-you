eda100656dff9494e5eb13133236bcb1dd9e460f
andrey post <postoliuk.av@gmail.com>
2021-02-08 18:47:01 +0200

    set modals

--- src/js/index.js
+++ src/js/index.js
@@ -13,5 +13,38 @@ document.addEventListener('click', (e) => {
 })
 feedbackForm.onsubmit = (e) => {
     e.preventDefault()
+    const showMessage = (message) => {
+        let canvas = document.querySelector('.canvas'),
+            modal = document.querySelector('.modal')
+
+        canvas.classList.add('active')
+        modal.classList.add('active')
+
+        canvas.onclick = (e) => {
+            canvas.classList.remove('active')
+            modal.classList.remove('active')
+        }
+        modal.querySelector('.close').onclick = (e) => {
+            canvas.classList.remove('active')
+            modal.classList.remove('active')
+        }
+
+        modal.querySelector('p').innerHTML = message
+    }
+    fetch('/submit', {
+    // fetch('https://my-json-server.typicode.com/andreypost/db/posts', {
+        // fetch(`[https://randomuser.me/api/?results=${10}]`, {
+        method: 'POST',
+        body: new FormData(feedbackForm)
+    })
+        .then(response => {
+            console.log(response)
+            if (response.ok) {
+                showMessage('Мы с вами свяжемся в ближайшее время!')
+            } else {
+                showMessage('Сейчас невозможно отправить Ваши данные, попробуйте позже!')
+            }
+        })
+        .catch(console.error())
     feedbackForm.reset()
 }
