a8f1b5816206e5a571db9ba07c65736640c87591
andrey post <postoliuk.av@gmail.com>
2021-02-10 09:39:34 +0200

    optimisation css

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
+let  modal = document.querySelector('.modal')
 document.addEventListener('click', (e) => {
-    if (e.target.closest('.scrollTo') && !e.target.parentNode.classList.contains('active')) {
+    if (e.target.dataset.section && !e.target.parentNode.classList.contains('active')) {
         let target = e.target
         window.scrollBy({
             top: document.querySelector(`.${target.dataset.section}`).getBoundingClientRect().top,
@@ -9,22 +10,20 @@ document.addEventListener('click', (e) => {
             target.closest('.points').querySelector('.active').classList.remove('active')
             target.parentNode.classList.add('active')
         }
-    }
+    } else if (e.target === modal || e.target.closest('.close')) modal.classList.remove('active')
 })
 feedbackForm.onsubmit = (e) => {
     e.preventDefault()
     const showMessage = (message, color) => {
-        let  modal = document.querySelector('.modal')
+        // let  modal = document.querySelector('.modal')
         modal.classList.add('active')
         modal.querySelector('p').style.color = color
         modal.querySelector('p').innerHTML = message
-        modal.onclick = (e) => {
-            if (e.target === modal || e.target.closest('.close')) modal.classList.remove('active')
-        }
+        // modal.onclick = (e) => {
+            // if (e.target === modal || e.target.closest('.close')) modal.classList.remove('active')
+        // }
     }
     fetch('/submit', {
-    // fetch('https://my-json-server.typicode.com/andreypost/db/posts', {
-        // fetch(`[https://randomuser.me/api/?results=${10}]`, {
         method: 'POST',
         body: new FormData(feedbackForm)
     })
